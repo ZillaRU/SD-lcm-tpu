07f07ce916da06938f2e60e77de8367212d00beb
ZillaRU <zilla_ru@zju.edu.cn>
2024-08-21 10:46:34 +0800

    fix

--- prepare.sh
+++ prepare.sh
@@ -9,6 +9,6 @@ fi
 if [ ! -d "./models/controlnet" ]; then
     mkdir models/controlnet && python3 -m dfss --url=open@sophgo.com:/aigc/sd/canny_multize.bmodel && mv canny_multize.bmodel models/controlnet 
 fi
-if [ ! -d "./models/basic/awportrait" ]; then
+if [ ! -d "./models/basic/hellonijicute" ]; then
     python3 -m dfss --url=open@sophgo.com:/aigc/sd/hellonijicute.tgz && tar -xzvf hellonijicute.tgz && rm hellonijicute.tgz && mv hellonijicute models/basic/
 fi
