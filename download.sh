11ffc339af465a9ab5f4b92358d72c7051c4d211
ZillaRU <zilla_ru@zju.edu.cn>
2024-10-10 16:15:18 +0800

    add download.sh

---
+++ download.sh
@@ -0,0 +1,12 @@
+if [ ! -d "./models" ]; then
+    mkdir models
+fi
+if [ ! -d "./models/basic" ]; then
+    mkdir models/basic
+fi
+if [ ! -d "./models/controlnet" ]; then
+    mkdir models/controlnet && python3 -m dfss --url=open@sophgo.com:/aigc/sd/canny_multize.bmodel && mv canny_multize.bmodel models/controlnet 
+fi
+if [ ! -d "./models/basic/hellonijicute" ]; then
+    python3 -m dfss --url=open@sophgo.com:/aigc/sd/hellonijicute.tgz && tar -xzvf hellonijicute.tgz && rm hellonijicute.tgz && mv hellonijicute models/basic/
+fi
--- prepare.sh
+++ prepare.sh
@@ -1,14 +1,2 @@
 pip3 install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip3 install -U dfss
-if [ ! -d "./models" ]; then
-    mkdir models
-fi
-if [ ! -d "./models/basic" ]; then
-    mkdir models/basic
-fi
-if [ ! -d "./models/controlnet" ]; then
-    mkdir models/controlnet && python3 -m dfss --url=open@sophgo.com:/aigc/sd/canny_multize.bmodel && mv canny_multize.bmodel models/controlnet 
-fi
-if [ ! -d "./models/basic/hellonijicute" ]; then
-    python3 -m dfss --url=open@sophgo.com:/aigc/sd/hellonijicute.tgz && tar -xzvf hellonijicute.tgz && rm hellonijicute.tgz && mv hellonijicute models/basic/
-fi
